fae1c7ddc4fcb3dd620734e8e908a5dfea3139ae
Joshua Cavell <xclusive36@gmail.com>
2023-03-16 15:38:40 -0400

    Merge branch 'develop' of github.com:erin-m-keller/project-one into auth

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -24,51 +24,78 @@ function loadFirebase() {
           const auth = fireAuth.getAuth(app); // Get the auth object
 
           const signInForm = document.getElementById("sign-in-form"); // Get the sign-in form
+          const signUpForm = document.getElementById("sign-up-form"); // Get the sign-up form
           const userName = document.getElementById("user-name"); // Get the user name input
           const userEmail = document.getElementById("sign-in-user-email"); // Get the user email input
           const userPassword = document.getElementById("sign-in-user-password"); // Get the user password input
+          const userEmailSignup = document.getElementById("sign-up-user-email"); // Get the user email input
+          const userPasswordSignup = document.getElementById(
+            "sign-up-user-password"
+          ); // Get the user password input
 
           const signOutBtn = document.getElementById("sign-out"); // Get the sign-out button
           const signInBtnGoogle = document.getElementById("sign-in-google"); // Get the sign-in with Google button
 
-          const signInWithPassword = (email, password) => {
+          signUpForm.addEventListener("submit", (e) => {
+            // Add a submit event listener to the sign-up form
+            e.preventDefault(); // Prevent the form from submitting
+
+            const user = userEmailSignup.value.trim();
+            const password = userPasswordSignup.value.trim();
+
             fireAuth
-              .signInWithEmailAndPassword(auth, email, password)
+              .createUserWithEmailAndPassword(auth, email, password)
               .then((userCredential) => {
                 const user = userCredential.user;
-                return user;
+                console.log(user);
+                signOutBtn.style.display = "block"; // Show the sign-out button
               })
               .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
                 console.log(errorCode, errorMessage);
               });
-          }; // Sign in with email and password
+          });
 
           signInForm.addEventListener("submit", (e) => {
             // Add a submit event listener to the sign-in form
             e.preventDefault(); // Prevent the form from submitting
-            const user = signInWithPassword(
-              userEmail.value.trim(),
-              userPassword.value.trim()
-            ); // Sign in with the email and password
+            const email = userEmail.value.trim();
+            const password = userPassword.value.trim();
 
-            console.log(user);
-            signOutBtn.style.display = "block"; // Show the sign-out button
+            fireAuth
+              .signInWithEmailAndPassword(auth, email, password)
+              .then((userCredential) => {
+                const user = userCredential.user;
+
+                console.log(user);
+                signOutBtn.style.display = "block"; // Show the sign-out button
+              })
+              .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+              });
           });
 
           const provider = new fireAuth.GoogleAuthProvider(); // Create a Google provider object
 
           signInBtnGoogle.addEventListener("click", () => {
             // Add a click event listener to the sign-in button
-            fireAuth.signInWithPopup(auth, provider).then((result) => {
-              // Sign in with the Google provider object
-              const user = result.user; // Get the signed-in user from the result
+            fireAuth
+              .signInWithPopup(auth, provider)
+              .then((result) => {
+                // Sign in with the Google provider object
+                const user = result.user; // Get the signed-in user from the result
 
-              console.log(user);
-              signInBtnGoogle.style.display = "none"; // Hide the sign-in button
-              signOutBtn.style.display = "block"; // Show the sign-out button
-            });
+                console.log(user);
+                signOutBtn.style.display = "block"; // Show the sign-out button
+              })
+              .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(errorCode, errorMessage);
+              });
           });
 
           signOutBtn.addEventListener("click", () => {
@@ -76,7 +103,6 @@ function loadFirebase() {
             fireAuth.signOut(auth).then(() => {
               // Sign out
               console.log("User signed out");
-              signInBtn.style.display = "block"; // Show the sign-in button
               signOutBtn.style.display = "none"; // Hide the sign-out button
             });
           });
